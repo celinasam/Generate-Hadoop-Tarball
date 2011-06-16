46d163a47bc4fde5002bfb06ee6f4998523116c9
Celina d' Ávila Samogin <celinasam@yahoo.com>
2011-06-16 19:02:29 -0300

    ainda primeira versão

--- conf-raid.sh
+++ conf-raid.sh
@@ -10,10 +10,8 @@ mkdir $2/test/$VERSION/hdfs
 mkdir $2/test/$VERSION/hdfs/DFS/
 mkdir $2/test/$VERSION/hdfs/DFS/conf
 cp -Rp /home/hadoop/Download/hadoop-tarball/conf/raid.xml $2/test/$VERSION/hdfs/DFS/conf
-mkdir /$2/test/$VERSION/dfs1
-mkdir /$2/test/$VERSION/dfs2
 export HADOOP_HOME=$2/test/hadoop-$VERSION
 cd $HADOOP_HOME
-chmod +x $2/hadoop-mapreduce/src/contrib/raid/bin/*raidnode*.sh
-cp -Rp $2/hadoop-mapreduce/src/contrib/raid/bin/*raidnode*.sh bin
+chmod +x $HADOOP_HOME/mapred/src/contrib/raid/bin/*raidnode*.sh
+cp -Rp $HADOOP_HOME/mapred/src/contrib/raid/bin/*raidnode*.sh $HADOOP_HOME/bin
 cp -Rp $HADOOP_HOME/mapred/contrib/raid/hadoop-*-raid.jar $HADOOP_HOME/lib
