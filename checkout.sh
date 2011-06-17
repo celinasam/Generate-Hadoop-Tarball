be7645543bbc61c58540d0d5606a3e61e44f95e2
Celina d' Ávila Samogin <celinasam@yahoo.com>
2011-06-16 21:59:57 -0300

    ainda primeira versão

--- checkout.sh
+++ checkout.sh
@@ -9,9 +9,9 @@ export VERSION=$1
 export BRANCH=`echo ${VERSION:0:4}`
 
 cd $2
-svn checkout https://svn.apache.org/repos/asf/hadoop/common/branches/branch-$BRANCH hadoop-common > check-common.out 2>&1
-svn checkout https://svn.apache.org/repos/asf/hadoop/hdfs/branches/branch-$BRANCH hadoop-hdfs > check-hdfs.out 2>&1
-svn checkout https://svn.apache.org/repos/asf/hadoop/mapreduce/branches/branch-$BRANCH hadoop-mapreduce > check-mapreduce.out 2>&1
+svn checkout https://svn.apache.org/repos/asf/hadoop/common/branches/branch-$BRANCH/common hadoop-common > check-common.out 2>&1
+svn checkout https://svn.apache.org/repos/asf/hadoop/hdfs/branches/branch-$BRANCH/hdfs hadoop-hdfs > check-hdfs.out 2>&1
+svn checkout https://svn.apache.org/repos/asf/hadoop/mapreduce/branches/branch-$BRANCH/mapreduce hadoop-mapreduce > check-mapreduce.out 2>&1
 
 finish_time=$(date +%s)
 echo "Time duration: $(($((finish_time - start_time))/60)) min $(($((finish_time - start_time))%60)) secs."
